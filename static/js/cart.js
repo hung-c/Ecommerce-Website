b8cb06bb65366990b10761d0bdf9e5e221ff98ff
hcaobt <124621020+hcaobt@users.noreply.github.com>
2023-03-03 15:50:33 -0600

    [Add] Update Cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,5 +1,4 @@
 var updateBtns = document.getElementsByClassName('update-cart');
-
 for( i = 0; i < updateBtns.length;  i++)
 {
     updateBtns[i].addEventListener('click', function(){
@@ -13,8 +12,29 @@ for( i = 0; i < updateBtns.length;  i++)
         {
             console.log("User is not autheticated")
         }else{
-            console.log("User is autheticated, sending data")
-
+            
+            updateUserOrder(productId, action)
         }
     })
+}
+
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        }, 
+        body:JSON.stringify({'productId':productId, 'action':action})
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        location.reload()
+    });
 }
