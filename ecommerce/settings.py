40134cfbafd7d1066b901604569a1c20c0840258
hung-c <57050796+hung-c@users.noreply.github.com>
2023-05-04 17:09:30 -0500

    [Update] New FE

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store.apps.StoreConfig'
+    'store.apps.StoreConfig',
+    'ecommerce'
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR,'static') 
 ]
 
 MEDIA_URL = '/images/'
